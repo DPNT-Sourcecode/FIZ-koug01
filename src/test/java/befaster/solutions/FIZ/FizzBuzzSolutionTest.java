a5271df0ae689a336911900619b0e4c0b08bbe14
sbx_user1059 <sbx_user1059@ip-10-129-181-23.eu-west-2.compute.internal>
2018-10-23 20:34:24 +0000

    Tue Oct 23 20:34:24 UTC 2018

--- src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
+++ src/test/java/befaster/solutions/FIZ/FizzBuzzSolutionTest.java
@@ -16,4 +16,19 @@ public class FizzBuzzSolutionTest {
     public void fizzBuzzMultipleOfThree() {
         assertEquals("fizz", fizzBuzzSolution.fizzBuzz(3));
     }
+
+    @Test
+    public void fizzBuzzMultipleOfFive() {
+        assertEquals("buzz", fizzBuzzSolution.fizzBuzz(5));
+    }
+
+    @Test
+    public void fizzBuzzMultipleOfFifteen() {
+        assertEquals("fizz buzz", fizzBuzzSolution.fizzBuzz(30));
+    }
+
+    @Test
+    public void fizzBuzzNotAMultipleOfThreeFiveFifteen() {
+        assertEquals("19", fizzBuzzSolution.fizzBuzz(19));
+    }
 }
